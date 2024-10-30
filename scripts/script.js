b39866280aa08188d9bf24ad6ea87fffe37a2f0f
Brittney <149526365+bdaniel3@users.noreply.github.com>
2024-10-29 21:46:17 -0500

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -11,7 +11,6 @@ const games = [
     gameAuthor: "Brittney Daniel",
     gameAbout: "It's almost time for class, Drag Dr. Cumbie to the correct time.",
     gameRepo: "https://github.com/bdaniel3/timeTangle",
-    gameGroup: "7"
   },
   {
     gameName: "Square Game", 
@@ -19,7 +18,6 @@ const games = [
     gameAuthor: "Allison Lacy",
     gameAbout: "How many times can you click the square?",
     gameRepo: "https://github.com/AllisonLacy/alligamerepo",
-    gameGroup: "7"
   },
   {
     gameName: "Mario Coin Catcher",
@@ -27,7 +25,6 @@ const games = [
     gameAuthor: "Garrett Bouldin",
     gameAbout: "Drag Mario around the gamespace to catch coins and earn points.",
     gameRepo: "https://github.com/gbouldin43/vigilant-giggle-david",
-    gameGroup: "7"
   },
   {
     gameName: "Cow Game",
@@ -35,8 +32,175 @@ const games = [
     gameAuthor: "Spencer Blake",
     gameAbout: "Use the arrow keys to move the cow. Move 5 times to win!",
     gameRepo: "https://github.com/SblakeUNA/literate-octo-disco-david",
-    gameGroup: "7"
-  }
+  },
+   {
+    gameName: "Escape the Garden", 
+    gameURL: "https://baileestrait.github.io/dev-david-lab/", 
+    gameAuthor: "Bailee Strait",
+    gameAbout: "Click and drag the fairy to escape from the garden!",
+    gameRepo: "https://github.com/baileestrait/dev-david-lab",
+  },
+
+ {
+    gameName: "Vroom", 
+    gameURL: "https://jaxn-harvey.github.io/dev-david/", 
+    gameAuthor: "Jackson Harvey",
+    gameAbout: "Drag the race car across the finish line to win!",
+    gameRepo: "https://github.com/jaxn-harvey/dev-david",
+  },
+
+ {
+    gameName: "Catch the Butterfly", 
+    gameURL: "https://iarcher444.github.io/dev-david-lab/", 
+    gameAuthor: "Isabella Archer",
+    gameAbout: "Help drag the girl over to the butterfly so she can catch it and score some points!",
+    gameRepo: "https://github.com/iarcher444/dev-david-lab",
+  },
+
+ {
+    gameName: "The Emoji game", 
+    gameURL: "https://mtrejo03.github.io/dev-david-hw/", 
+    gameAuthor: "Milly Trejo",
+    gameAbout: "Match each emoji to its correct adjective by dragging it!",
+    gameRepo: "https://github.com/mtrejo03/dev-david-hw",
+  },
+  
+   {
+    gameName: "Futbol Mania", 
+    gameURL: "https://jm-2017.github.io/urban-carnival-nuExample/", 
+    gameAuthor: "Janet Martinez",
+    gameAbout: "Want to play some soccer? Simply click the soccer ball and drag it into the goalpost and score a golazo!",
+    gameRepo: "https://codepen.io/jm-2017/pen/YzmwxZj",
+  },
+
+  {
+    gameName: "Slides", 
+    gameURL: "https://basstbone.github.io/dev-david-hw/", 
+    gameAuthor: "Sean Oliver",
+    gameAbout: "Move the game piece using arrow keys and reach the target to score points!",
+    gameRepo: "https://github.com/basstbone/dev-david-hw",
+  },
+
+   {
+    gameName: "Justin’s Game", 
+    gameURL: "https://justinalkire.github.io/redesigned-fiesta/", 
+    gameAuthor: "Justin Alkire",
+    gameAbout: "Move the photo over the 'Hide Me' text to win.",
+    gameRepo: "https://github.com/JustinAlkire/redesigned-fiesta",
+  },
+
+  {
+    gameName: "Meh Game", 
+    gameURL: "https://mrspartan38.github.io/dev-david-hw/", 
+    gameAuthor: "Evan Sager",
+    gameAbout: "Drag the picture around to find the winning spot.",
+    gameRepo: "https://github.com/mrspartan38/dev-david-hw",
+  },
+
+  {
+    gameName: "colorMeh", 
+    gameURL: "https://mlsmason6.github.io/improved-computing-machine-colorName/", 
+    gameAuthor: "Mason Schrader",
+    gameAbout: "See what color correlates with your name!",
+    gameRepo: "https://github.com/MLSMason6/improved-computing-machine-colorName",
+  },
+
+  {
+    gameName: "Escape Room", 
+    gameURL: "https://joshuabrad03.github.io/scaling-octo-david/", 
+    gameAuthor: "Joshua Bradley",
+    gameAbout: "Find the code to escape!",
+    gameRepo: "https://github.com/joshuabrad03/scaling-octo-david",
+    },
+
+    {
+    gameName: "Circle Game", 
+    gameURL: " https://jomlli709.github.io/octo-goggles-david-hw/", 
+    gameAuthor: "Jodi Gambrell",
+    gameAbout: "Move the red circle into the green box to score!",
+    gameRepo: "https://github.com/jomlli709/octo-goggles-david-hw/tree/main",
+  },
+
+
+ {
+    gameName: "Shooting Star", 
+    gameURL: "https://nbrown2002.github.io/Dev-David/", 
+    gameAuthor: "Noah Brown",
+    gameAbout: "Drag the star across the screen!",
+    gameRepo: "https://github.com/Nbrown2002/Dev-David",
+  },
+
+ {
+    gameName: "UFO Game", 
+    gameURL: "https://whitb43.github.io/UFO-Escape/", 
+    gameAuthor: "Whit Brewer",
+    gameAbout: "Make it behind the black hole!",
+    gameRepo: "https://github.com/WhitB43/UFO-Escape",
+  },
+
+ {
+    gameName: "Battery Charger Game", 
+    gameURL: "https://ssasaki1.github.io/dev-david-lab/", 
+    gameAuthor: "Shun Sasaki",
+    gameAbout: "Charge the Battery to 100%!",
+    gameRepo: "https://github.com/ssasaki1/dev-david-lab",
+  },
+
+   {
+    gameName: "WOW Game!", 
+    gameURL: "https://jonathan-hall34.github.io/scaling-pancake-dev-david-game/", 
+    gameAuthor: "Jonathan Hall",
+    gameAbout: "Move the piece into the green box to score!",
+    gameRepo: "https://github.com/Jonathan-Hall34/scaling-pancake-dev-david-game",
+  },
+
+  {
+    gameName: "Tic Tac Toe", 
+    gameURL: "https://lmoats11.github.io/Dev-David-TicTac/", 
+    gameAuthor: "Luke Moats",
+    gameAbout: "Tic Tac Toe",
+    gameRepo: "https://github.com/LMoats11/Dev-David-TicTac",
+     },
+
+  {
+    gameName: "Rock-Paper-Scissor", 
+    gameURL: "https://kethanpilla.github.io/dev-freddie-hw/", 
+    gameAuthor: "Kethan Pilla",
+    gameAbout: "Rock Paper Scissors",
+    gameRepo: "https://github.com/KethanPilla/dev-freddie-hw",
+  },
+
+{
+    gameName: "Wow Game", 
+    gameURL: "https://pmalhotra21.github.io/wow-game/", 
+    gameAuthor: "Paridhi Malhotra",
+    gameAbout: "Move the piece and collect points!",
+    gameRepo: "https://github.com/pmalhotra21/wow-game",
+  },
+
+  {
+    gameName: "Spongebob vs Doodlebob", 
+    gameURL: "https://a-nunley.github.io/dev-david/", 
+    gameAuthor: "Alex Nunley",
+    gameAbout: "Get spongebob away from doodlebob!",
+    gameRepo: "https://github.com/a-nunley/dev-david",
+  },
+
+{
+    gameName: "David V Goliath", 
+    gameURL: "https://germannnick.github.io/Dev-David-Goliath/", 
+    gameAuthor: "Nick Germann",
+    gameAbout: "Use the rock in David's sling to knock Goliath in the head.",
+    gameRepo: "https://github.com/germannnick/Dev-David-Goliath",
+  },
+
+{
+    gameName: "Find the Surprise!", 
+    gameURL: "https://tuesdayannb.github.io/miniature-enigma-david/", 
+    gameAuthor: "Tuesday Brown",
+    gameAbout: "Find the hidden surprise.",
+    gameRepo: "https://github.com/TuesdayAnnB/miniature-enigma-david",
+  },
 ];
 
 // Function to generate cards dynamically
